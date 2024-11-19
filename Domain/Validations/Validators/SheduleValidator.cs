67b9a8bf7fd704d289965be7dfc099bebfd92593
Arthur <gera.gde.dom@list.ru>
2024-11-19 13:18:08 +0200

    AddValidation

---
+++ Domain/Validations/Validators/SheduleValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Primitives;
+using FluentValidation;
+
+namespace Domain.Validations.Validators;
+public class SheduleValidator: AbstractValidator<Shedule>
+{
+    public SheduleValidator()
+    {
+        RuleFor(shedule => shedule.DayOfWeek)
+            .NotEmpty().WithMessage(ValidationMessages.NotEmpty)
+            .NotNull().WithMessage(ValidationMessages.NotNull);
+        RuleFor(shedule => shedule.Lessons)
+            .NotNull().WithMessage(ValidationMessages.NotNull);
+        RuleFor(shedule => shedule.IsOddWeek)
+            .NotNull().WithMessage(ValidationMessages.NotNull);
+    }
+}
