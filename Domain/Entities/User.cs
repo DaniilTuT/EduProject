0808772b00e6edb5231f3e55f5bc15f6a45f514d
Arthur <gera.gde.dom@list.ru>
2024-11-26 12:49:06 +0200

    Added Validation for user

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,14 +1,22 @@
-﻿namespace Domain.Entities;
+﻿using Domain.Validations.Validators;
+using Microsoft.Extensions.Options;
+
+namespace Domain.Entities;
 
 public class User : BaseEntity
 {
     public string Name { get; set; } = "Студент";
     public string Email { get; set; }
     public string Group  { get; set; }
-    public bool IsVerified { get; set; }
+    public bool IsVerified { get; set; } = false;
 
     public ICollection<VerificationCode> VerificationCodes { get; set; } = new List<VerificationCode>();
 
+    private void Validate()
+    {
+        var validate = new UserValidator();
+        validate.Validate(this);
+    }
     public void Update(User user)
     {
         Name = user.Name;
@@ -17,5 +25,12 @@ public class User : BaseEntity
         IsVerified = user.IsVerified;
     }
     //post Email -> письмо на емэйл с ссылкой на подтверждение ->update перейдя по ссылке в бд емэйл отмечается как верефицированный? -> с него можно зайти
-    
+    public User(string name, string email, string group, bool isVerified)
+    {
+        Name = name;
+        Email = email;
+        Group = group;
+        IsVerified = isVerified;
+        Validate();
+    }
 }
