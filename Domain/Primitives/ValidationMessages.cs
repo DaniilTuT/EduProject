efc140e0cd73bdaae8ca401b224cea579cd12f20
Arthur <gera.gde.dom@list.ru>
2024-11-26 12:49:06 +0200

    Added Validation for user

--- Domain/Primitives/ValidationMessages.cs
+++ Domain/Primitives/ValidationMessages.cs
@@ -5,4 +5,5 @@ public static class ValidationMessages
     public static string NotEmpty = "{PropertyName} не заполнен.";
     public static string WrongLength="{PropertyName} должен быть не меньше {MinLength} и не больше {MaxLength}.";
     public static string WrongTime = "{PropertyName} не может быть меньше чем конец.";
+    public static string WrongEmail = "{PropertyName} не является E-mail адрессом";
 }
